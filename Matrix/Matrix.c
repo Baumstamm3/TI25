37531987faa4abe81bc7dfeffb6229c32aa530a6
Baumstamm3 <Terrarion@web.de>
2025-11-07 14:41:00 +0100

    does not work. dont know why

--- Matrix/Matrix.c
+++ Matrix/Matrix.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 
-int main(){
-  int matrix1[] = {
-  {1,2},
-  {3,4},
-  {5,6}
-};
+int transponieren(int matrix[][2]){
+    int ergebnis[sizeof(matrix[0]) / sizeof(matrix[0][0])][sizeof(matrix) / sizeof(matrix[0])];
+  int length = sizeof(matrix) / sizeof(matrix[0]);
 
-int matrix2[] = transponieren(matrix1)
-  
-for(int i = 0; i < sizeof(matrix2)/sizeof(matrix[0]); i++) {
-    for(int j = 0; j < sizeof(matrix2[0])/sizeof(matrix[0][0]); j++) {
-        printf("%d ", array[i][j]);
-    }
-    printf("\n");
-} 
-}
-
-int transponieren(int matrix){
-  int ergebnis[]
-  
-  for(int i = 0; i < sizeof(matrix[0]) / sizeof(matrix[0][0]); i++){
-    ergebnis[i] = {}
-  }
-
-  int length = sizeof(matrix) / sizeof(matrix[0])
-  
   for(int i = 0; i < length; i++){
     for(int j = 0; j < sizeof(matrix[i]) / sizeof(matrix[i][0]); j++){
       ergebnis[j][i] = matrix[i][j]
     }
   }
-  return ergebnis
+  return ergebnis;
+}
+
+int main(){
+  	int matrix1[3][2] = {
+  		{1,2},
+  		{3,4},
+  		{5,6}
+	  };
+
+int matrix2[2][3] = transponieren(matrix1);
+  
+  for(int i = 0; i < 2; i++) {
+      for(int j = 0; j < 3; j++) {
+          printf("%d ", matrix2[i][j]);
+      }
+      printf("\n");
+  } 
 }
