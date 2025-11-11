d1372b630c23ed148ed6229b777f62577fcf67c0
Baumstamm3 <Terrarion@web.de>
2025-11-11 13:08:02 +0100

    WIA ggt aus Struktogram

---
+++ ggt/index.js
@@ -0,0 +1,11 @@
+function main(a,b){
+    if(a < 1 || b < 1){return `So funtioniert das mit der Mathematik nicht!`}
+        do{
+            if(a > b){
+                a -= b
+            }else{
+                b -= a
+            }
+        }while(b > 0)
+    return a
+}
