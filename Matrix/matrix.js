67c0e44a83f34450c53a12391a419cf6d56d18a5
Baumstamm3 <Terrarion@web.de>
2025-11-13 14:37:22 +0100

    Matizenmultiplikation

---
+++ Matrix/matrix.js
@@ -0,0 +1,56 @@
+class Matrix {
+    constructor(){
+        this.matrix
+    }
+
+    transpone(matrixA){
+
+    }
+
+    static mul(matrixA, matrixB){
+        let aM = matrixA.length
+        let aN = matrixA[0].length
+
+        let bM = matrixB.length
+        let bN = matrixB[0].length
+
+        if(aN != bM){
+            return undefined
+        }
+
+        let matrixC = []
+
+        for(let cM = 0; cM < aM; cM++){
+            matrixC[cM] = []
+            for(let cN = 0; cN < bN; cN++){
+                let result = 0
+
+                for(let i = 0; i < aM; i++){
+                    result += matrixA[cN][i] * matrixB[i][cN]
+                }
+
+                matrixC[cM][cN] = result
+            }
+        }
+
+        return matrixC
+    }
+
+    get trans(){
+
+    }
+}
+
+let matrix1 = [
+    [3,2,1],
+    [1,0,2]
+]
+
+let matrix2 = [
+    [1,2],
+    [0,1],
+    [4,0]
+]
+
+let matrix3 = Matrix.mul(matrix1, matrix2)
+console.log(String(matrix3))
