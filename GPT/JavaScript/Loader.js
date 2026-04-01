25d1aa1a8d366bf800f8210df11a2e94b6d1fed8
Baumstamm3 <terrarion@web.de>
2026-04-01 13:45:26 +0200

    This is getting out of hand. now there's two of them.

--- GPT/JavaScript/Loader.js
+++ GPT/JavaScript/Loader.js
@@ -1,6 +1,6 @@
 const USER = "Baumstamm3"
 const REPOSITORY = "TI25"
-const DIRECTORY = "JavaScript/Executables"
+const DIRECTORY = "GPT/JavaScript/Executables"
 const ACCESS_TOKEN = "github_pat_" + "11BWUI2XA0yd3GADKMC1rh_DbqWeE6by7gO00zWtoXguHNNcVL2v2dYL2DY9liyYe4OYW2GRUN2i2xar67"
 
 const HEADER  = new Headers()
@@ -10,32 +10,75 @@ const FETCH_REQUEST = {
 	headers: HEADER
 }
 
-async function acquireData(map, names ,folder){
+const NAMES = []
+const MODULES = new Map()
+
+main()
+
+function main(){
+	acquireModules()
+}
+
+function acquireModules(){
 	
-	let contents = await gitFetchContents(`StupidChess`,folder)
-	console.log(contents)
-	
-	for(let i = 0; i < contents.length; i++){
-		names[i] = contents[i].name.split(".", 1)[0]
-		map.set(
-			names[i],
-			await gitFetchJSON(`StupidChess`, contents[i].path)
-		)
-		console.log(JSON.stringify(classes.get(classesNames[i])))
+	gitHubFetchPath(DIRECTORY)
+	.then( contents => {
+		console.log(contents)
+		for(let i = 0; i < contents.length; i++){
+			NAMES[i] = contents[i].name.split(".", 1)[0]
+			gitHubFetchContentRaw(contents[i].path)
+			.then( rawModule => {createModuleObject(rawModule)})
+			.then( parsedModule => {
+				MODULES.set(
+					NAMES[i],
+					parsedModule
+				)
+			})
+		}
+	}).then(() => {
+		console.log(NAMES)
+		console.log(MODULES)
+	})
+}
+
+function gitHubFetchPath(path){
+	console.log(`Path: https://api.github.com/repos/${USER}/${REPOSITORY}/contents/${path}`)
+	return fetch(
+		//Test-Link: https://api.github.com/repos/Baumstamm3/TI25/contents/GPT/JavaScript/Executables
+		`https://api.github.com/repos/${USER}/${REPOSITORY}/contents/${path}`,
+		FETCH_REQUEST)
+	.then(data => data.json())
+}
+
+function gitHubFetchContentParsed(path){
+	return gitHubFetchPath(path)
+	.then(encoded => atob(encoded.content) )
+	.then(decoded => JSON.parse(decoded))
+}
+
+function gitHubFetchContentRaw(path){
+	return gitHubFetchPath(path)
+	.then(rawBase64 => rawBase64.content)
+}
+
+function decodeBase64Module(moduleString){
+	snippets = moduleString.split("\\n")
+	result = ""
+	for(let i = 0; i < snippets.length; i++){
+		result += atob(snippets[i]) + "\n"
 	}
 }
 
-async function gitHubFetchDirectory(repo, folder){
-	let data = await fetch(
-		`https://api.github.com/repos/${USER}/${REPOSITORY}/contents/${DIRECTORY}`,
-		FETCH_REQUEST
-	)
-	 
-	return await data.json()
-}
-
-async function gitFetchJSON(repo, path){
-	let data = await gitFetchContents(repo, path)
+function createModuleObject(moduleData) {
 	
-	return JSON.parse( atob( data.content ) )
+ 	if(globalThis.URL.createObjectURL){
+    	const blob = new Blob([decodeBase64Module(moduleData)], { type: 'text/javascript' })
+    	const url = URL.createObjectURL(blob)
+    	let result = import(url)
+		.then(URL.revokeObjectURL(url))
+		return result
+	}
+  
+  	const url = "data:text/javascript;base64," + moduleData
+  	return import(url)
 }
